db32efd9884b07ed4d5143d1ed3077c3dc35f5ed
EvidenceEkanem <ekanemevidence@gmail.com>
2019-03-22 10:59:50 +0100

    commit

--- src/resources/views/layouts/layout.blade.php
+++ src/resources/views/layouts/layout.blade.php
@@ -4,13 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
     <link rel="stylesheet" href="css/app.css">
     <link href="css/style.css" rel="stylesheet">
-=======
-    <link rel="stylesheet" href="vendor/laravelTodoList/css/app.css">
-    <link href="vendor/laravelTodoList/css/style.css" rel="stylesheet">
->>>>>>> 63856cf281522b73850e53cb198e8c8e2a1f6fcf
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title>Tasks</title>
 </head>
--- src/resources/views/tasks.blade.php
+++ src/resources/views/tasks.blade.php
@@ -155,11 +155,7 @@
 @endsection
 
 @section('script')
-<<<<<<< HEAD
 <script src="js/axios/axios.min.js"></script>
-=======
-<script src="vendor/laravelTodoList/js/axios/axios.min.js"></script>
->>>>>>> 63856cf281522b73850e53cb198e8c8e2a1f6fcf
 
 <script>
     
